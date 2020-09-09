50e7eef1f1c3f092fcf547fc7eff7a631118f196
zlzhaoe <18192517977@163.com>
2020-09-09 17:24:14 +0800

    约车统计

--- src/java/com/ordercar/vo/RemainderDetailVo.java
+++ src/java/com/ordercar/vo/RemainderDetailVo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class RemainderDetailVo {
+    private String id;//预约时段ID
     private String startTime;//开始时间
     private String endTime;//结算时间
     private String reservedNumber;//已预约
